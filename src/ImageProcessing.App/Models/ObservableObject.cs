bc1269bbacbeba8a342dffc17ccbec4345ca7750
piotr <piotr.informatyka@gmail.com>
2021-03-01 12:36:36 +0100

    add simple view in WPF app

---
+++ src/ImageProcessing.App/Models/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ImageProcessing.App.Models
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
